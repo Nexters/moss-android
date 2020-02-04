212013a267816814928d64a278f1d688f8cec26d
YChaeeun <se_same5315@naver.com>
2020-02-05 01:30:06 +0900

    feat-#6 [케익다이어리] alert dialog activity에 연결하기
    - 커스텀 해야 함!
    - 클릭 이벤트 처리 해야 함

--- app/src/main/java/com/nexters/moss/ui/receive/ReceiveActivity.kt
+++ app/src/main/java/com/nexters/moss/ui/receive/ReceiveActivity.kt
@@ -1,11 +1,12 @@
 package com.nexters.moss.ui.receive
 
 import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import com.nexters.moss.R
 import com.nexters.moss._base.BaseActivity
 import com.nexters.moss.databinding.ActivityReceiveBinding
+import kotlinx.android.synthetic.main.activity_receive.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ReceiveActivity : BaseActivity<ActivityReceiveBinding>() {
@@ -17,6 +18,31 @@ class ReceiveActivity : BaseActivity<ActivityReceiveBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        btn_receive_report.setOnClickListener {
+            showDialog()
+        }
+
+    }
+
+    private fun showDialog() {
+        AlertDialog.Builder(
+            ContextThemeWrapper(
+                this@ReceiveActivity,
+                R.style.Theme_AppCompat_Light_Dialog
+            )
+        ).apply {
+            setView(layoutInflater.inflate(R.layout.dialog_report, null))
+
+            setPositiveButton("확인") { _, _ ->
+                toast("alert ok")
+            }
+
+            setNegativeButton("취소") { _, _ ->
+                toast("alert cancel")
+            }
+
+            show()
+        }
 
     }
 
