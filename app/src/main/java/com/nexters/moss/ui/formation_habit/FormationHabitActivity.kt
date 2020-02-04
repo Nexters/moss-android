a5ba64b7b038148bc79186f4d970687efbe371f2
sodp5 <sodp5@naver.com>
2020-02-04 19:08:43 +0900

    feat-#18/formation_habit adapter 연결

--- app/src/main/java/com/nexters/moss/ui/formation_habit/FormationHabitActivity.kt
+++ app/src/main/java/com/nexters/moss/ui/formation_habit/FormationHabitActivity.kt
@@ -3,9 +3,12 @@ package com.nexters.moss.ui.formation_habit
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.GridLayoutManager
 import com.nexters.moss.R
 import com.nexters.moss._base.BaseActivity
 import com.nexters.moss.databinding.ActivityFormationHabitBinding
+import com.nexters.moss.ui.formation_habit.adapter.FormationHabitListAdapter
+import com.nexters.moss.ui.formation_habit.adapter.FormationHabitListDecoration
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FormationHabitActivity : BaseActivity<ActivityFormationHabitBinding>() {
@@ -19,5 +22,17 @@ class FormationHabitActivity : BaseActivity<ActivityFormationHabitBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setupFormationHabitRecyclerView()
+    }
+
+    private fun setupFormationHabitRecyclerView() {
+        with(binding.rvFormationHabitList) {
+            adapter = FormationHabitListAdapter().apply {
+                refreshItemList(vm.formationHabitList)
+            }
+            layoutManager = GridLayoutManager(this@FormationHabitActivity, 2)
+            addItemDecoration(FormationHabitListDecoration())
+        }
     }
 }
--- app/src/main/java/com/nexters/moss/ui/formation_habit/FormationHabitViewModel.kt
+++ app/src/main/java/com/nexters/moss/ui/formation_habit/FormationHabitViewModel.kt
@@ -3,4 +3,8 @@ package com.nexters.moss.ui.formation_habit
 import androidx.lifecycle.ViewModel
 
 class FormationHabitViewModel : ViewModel() {
+    val formationHabitList = arrayListOf(
+        "물마시기", "스트레칭", "명상", "산책",
+        "뉴스보기", "아침식사", "일기쓰기", "책읽기"
+    )
 }
