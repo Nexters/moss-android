a486f675c39fd9059221bf1d58232c6eda9f5cf2
sodp5 <sodp5@naver.com>
2020-02-12 21:09:34 +0900

    feat-#18/formation_habit 습관 목록 별로 케이크 적용

--- app/src/main/java/com/nexters/moss/constant/HabitListConstant.kt
+++ app/src/main/java/com/nexters/moss/constant/HabitListConstant.kt
@@ -7,14 +7,14 @@ enum class HabitListConstant(
     private val habitName: String,
     @DrawableRes private val imageRes: Int
 ) {
-    DRINK_WATER("물마시기", R.drawable.icon_category_cake_greentea),
-    STRETCHING("스트레칭", R.drawable.icon_category_cake_greentea),
-    MEDITATION("명상", R.drawable.icon_category_cake_greentea),
+    DRINK_WATER("물마시기", R.drawable.icon_category_cake_watermelon),
+    STRETCHING("스트레칭", R.drawable.icon_category_cake_cheese),
+    MEDITATION("명상", R.drawable.icon_category_cake_cream),
     WALK("산책", R.drawable.icon_category_cake_greentea),
-    WATCH_NEWS("뉴스보기", R.drawable.icon_category_cake_greentea),
-    HAVE_BREAKFAST("아침식사", R.drawable.icon_category_cake_greentea),
-    WRITE_DIARY("일기쓰기", R.drawable.icon_category_cake_greentea),
-    READ_BOOK("책읽기", R.drawable.icon_category_cake_greentea);
+    WATCH_NEWS("뉴스보기", R.drawable.icon_category_cake_coffee),
+    HAVE_BREAKFAST("아침식사", R.drawable.icon_category_cake_apple),
+    WRITE_DIARY("일기쓰기", R.drawable.icon_category_cake_chestnut),
+    READ_BOOK("책읽기", R.drawable.icon_category_cake_);
 
     fun getValue() = habitName
 
