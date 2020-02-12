bc9879eb0ab329aafc438f161980e93030e209ac
YChaeeun <se_same5315@naver.com>
2020-02-12 19:51:25 +0900

    feat-#26 [케익보내기] 선택된 아이템 한 개 배경 변경
    - change background resource of selected recycler view item

--- app/src/main/java/com/nexters/moss/ui/send/adapter/SendAdapter.kt
+++ app/src/main/java/com/nexters/moss/ui/send/adapter/SendAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.nexters.moss.R
 import com.nexters.moss.model.CakeModel
@@ -13,23 +14,41 @@ class SendAdapter :
     RecyclerView.Adapter<SendAdapter.Holder>() {
     private var context: Context? = null
     private var cakeList = ArrayList<CakeModel>()
+    private var lastView: View? = null
 
-    inner class Holder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val habit = itemView.findViewById<TextView>(R.id.txt_send_recycler_habit)
+        private var itemBackground = itemView.findViewById<View>(R.id.layout_send_cake_choice)
 
-        fun bind(unit : CakeModel) {
+        fun bind(unit: CakeModel) {
             habit?.text = unit.habit
+        }
+
+        fun changeSelectedBackground() {
+            itemView.setOnClickListener {
+                Toast.makeText(context, " " + adapterPosition, Toast.LENGTH_SHORT).show()
+
+                if (lastView != null) {
+                    lastView!!.setBackgroundResource(R.drawable.send_item_default)
+                }
+
+                itemBackground.setBackgroundResource(R.drawable.send_item_selected)
+                lastView = itemBackground
+
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         context = parent.context
-        val view = LayoutInflater.from(context).inflate(R.layout.item_send_select, parent,false)
+        val view = LayoutInflater.from(context).inflate(R.layout.item_send_select, parent, false)
         return Holder(view)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
         holder.bind(cakeList[position])
+        holder.changeSelectedBackground()
     }
 
     override fun getItemCount(): Int {
