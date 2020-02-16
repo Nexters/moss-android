94f340321ca7e2b831514f114d447af5c87e49cf
sodp5 <sodp5@naver.com>
2020-02-16 22:50:43 +0900

    5refactor-#58/main_gui 첫 선물 케이크 팝업 전체케익 리스트 보여주기

--- app/src/main/java/com/nexters/moss/ui/dialog_first_gift/adapter/CakeListAdapter.kt
+++ app/src/main/java/com/nexters/moss/ui/dialog_first_gift/adapter/CakeListAdapter.kt
@@ -4,12 +4,30 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nexters.moss.R
 
 class CakeListAdapter : RecyclerView.Adapter<CakeListAdapter.CakeListViewHolder>() {
-    private var itemList = ArrayList<String>()
     private lateinit var context: Context
+
+    private val images = arrayOf(
+        R.drawable.icon_category_cake_watermelon,
+        R.drawable.icon_category_cake_cheese,
+        R.drawable.icon_category_cake_cream,
+        R.drawable.icon_category_cake_greentea,
+        R.drawable.icon_category_cake_coffee,
+        R.drawable.icon_category_cake_apple,
+        R.drawable.icon_category_cake_chestnut,
+        R.drawable.icon_category_cake_
+    )
+
+    private val names = arrayOf(
+        "수박케익", "치즈케익", "생크림케익",
+        "녹차케익", "커피케익", "사과케익",
+        "밤케익", "아몬드케익"
+    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CakeListViewHolder {
         context = parent.context
@@ -21,13 +39,16 @@ class CakeListAdapter : RecyclerView.Adapter<CakeListAdapter.CakeListViewHolder>
         )
     }
 
-    override fun getItemCount() = 9
+    override fun getItemCount() = names.size
 
     override fun onBindViewHolder(holder: CakeListViewHolder, position: Int) {
+        holder.cakeImage.setImageResource(images[position])
+        holder.cakeName.text = names[position]
     }
 
 
     class CakeListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val cakeImage: ImageView = itemView.findViewById(R.id.iv_cakeImage)
+        val cakeName: TextView = itemView.findViewById(R.id.tv_cakeName)
     }
 }
