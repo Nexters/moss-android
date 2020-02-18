68ccb5d1c1b8b4451b6d39c85d20f4166809fb63
sodp5 <sodp5@naver.com>
2020-02-18 17:55:25 +0900

    feat-#69/nickname 닉네임 입력시 시작하기 버튼 활성화

--- app/src/main/java/com/nexters/moss/ui/make_nickname/MakeNicknameViewModel.kt
+++ app/src/main/java/com/nexters/moss/ui/make_nickname/MakeNicknameViewModel.kt
@@ -1,7 +1,9 @@
 package com.nexters.moss.ui.make_nickname
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MakeNicknameViewModel : ViewModel() {
-
+    val nickname = MutableLiveData<String>("")
 }
