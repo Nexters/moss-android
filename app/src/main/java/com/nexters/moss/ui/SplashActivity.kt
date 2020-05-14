ccc7574b1d83e2820cb7e84392f9ef70b6f6fad4
sodp5 <sodp5@naver.com>
2020-05-14 20:40:34 +0900

    feat-#112/kakao_login 스플래쉬에서 자동로그인 기능 구현

--- app/src/main/java/com/nexters/moss/ui/SplashActivity.kt
+++ app/src/main/java/com/nexters/moss/ui/SplashActivity.kt
@@ -4,21 +4,27 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kakao.auth.Session
 import com.nexters.moss.R
 import com.nexters.moss.constant.SharedPreferenceConstant
 import com.nexters.moss.extension.getUserSharedPreference
 import com.nexters.moss.repository.UserRepository
 import com.nexters.moss.ui.formation_habit.FormationHabitActivity
 import com.nexters.moss.ui.main.MainActivity
+import com.nexters.moss.ui.make_nickname.MakeNicknameActivity
 import com.nexters.moss.ui.onboarding.OnboardingActivity
 import com.nexters.moss.utils.DLog
 import com.nexters.moss.utils.KakaoLoginUtils
+import com.nexters.moss.utils.KakaoSessionCallback
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class SplashActivity : AppCompatActivity() {
+    private var autoLogin = false
+
+    private val callback: KakaoSessionCallback by inject()
     private val userRepo: UserRepository by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,31 +33,55 @@ class SplashActivity : AppCompatActivity() {
 
         GlobalScope.launch {
             delay(1000)
+            setupKakaoCallback()
 
-            startActivity(Intent(applicationContext, OnboardingActivity::class.java))
+//            startActivity(Intent(applicationContext, OnboardingActivity::class.java))
+//            finish()
+        }
+    }
+
+    private fun setupKakaoCallback() {
+        callback.setOnSessionOpenedCallback {
+            startNicknameActivityWithId()
+        }
+        Session.getCurrentSession().addCallback(callback)
+        autoLogin = Session.getCurrentSession().checkAndImplicitOpen()
+        DLog.d("is Can Open: $autoLogin")
+    }
+
+    private fun startNicknameActivityWithId() {
+        GlobalScope.launch {
+            val id = KakaoLoginUtils.getAccessToken()
+            if (autoLogin) {
+                DLog.d("auto!")
+                saveHabikeryTokenInSharedPreference()
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+            } else {
+                startActivity(
+                    Intent(
+                        applicationContext,
+                        MakeNicknameActivity::class.java
+                    ).apply {
+                        putExtra(MakeNicknameActivity.EXTRA_KAKAO_ID, id)
+                    }
+                )
+            }
             finish()
         }
     }
 
-    private suspend fun isExistId(): Boolean {
+    private suspend fun saveHabikeryTokenInSharedPreference() {
         val sp = getUserSharedPreference()
 
-        val accessToken = sp.getString(
-            SharedPreferenceConstant.ACCESS_TOKEN.getValue(),
-            null
-        ) ?: SharedPreferenceConstant.UNKNOWN.getValue()
+        val accessToken = Session.getCurrentSession().tokenInfo.accessToken
 
-        val isExist = accessToken != SharedPreferenceConstant.UNKNOWN.getValue()
+        val token = userRepo.login(accessToken)
 
-        if (isExist) {
-            val token = userRepo.login(accessToken)
-            DLog.d("token = ${token.habikeryToken ?: ""}")
-            sp.edit().putString(
+        sp.edit().run{
+            putString(
                 SharedPreferenceConstant.HABIKERY_TOKEN.getValue(),
                 token.habikeryToken
-            ).apply()
-        }
-
-        return isExist
+            )
+        }.apply()
     }
 }
