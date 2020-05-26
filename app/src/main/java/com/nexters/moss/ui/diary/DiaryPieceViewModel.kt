b4f3ca6375649943d782ca046c045ea1ebca814b
sodp5 <sodp5@naver.com>
2020-05-26 21:50:41 +0900

    feat-#120/diary_piece 다이어리 비어있는 이미지 표시

--- app/src/main/java/com/nexters/moss/ui/diary/DiaryPieceViewModel.kt
+++ app/src/main/java/com/nexters/moss/ui/diary/DiaryPieceViewModel.kt
@@ -8,24 +8,28 @@ import com.nexters.moss.model.DiaryCakeModel
 import com.nexters.moss.model.DiaryModel
 import com.nexters.moss.repository.DiaryRepository
 import com.nexters.moss.utils.DLog
+import com.nexters.moss.utils.SingleLiveEvent
 import kotlinx.coroutines.launch
 
 class DiaryPieceViewModel(private val diaryRepo: DiaryRepository) : ViewModel() {
-
     private val _cakeList = MutableLiveData<List<DiaryModel>>(ArrayList())
     val cakeList: LiveData<List<DiaryModel>> get() = _cakeList
+
+    private val _isEmpty = MutableLiveData<Boolean>()
+    val isEmpty: LiveData<Boolean> get() = _isEmpty
 
     fun setCakeList(habikeryToken: String) {
         viewModelScope.launch {
             val response =  diaryRepo.getPieceOfCakeDiary(habikeryToken).data as ArrayList
             val itemList : ArrayList<DiaryModel> = ArrayList()
 
-            for (item in response){
+            for (item in response) {
                 if (item.count !=0){
                     itemList.add(item)
                 }
             }
 
+            _isEmpty.value = itemList.isEmpty()
             _cakeList.value = itemList
 
             //DLog.d("정보정보"+_cakeList.value.toString())
