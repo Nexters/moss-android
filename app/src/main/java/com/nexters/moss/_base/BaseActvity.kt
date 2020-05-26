76d2c74b5c5b3fac55192b07a16198364396c7b0
sodp5 <sodp5@naver.com>
2020-05-26 22:29:33 +0900

    status bar color base 변경

--- app/src/main/java/com/nexters/moss/_base/BaseActvity.kt
+++ app/src/main/java/com/nexters/moss/_base/BaseActvity.kt
@@ -2,15 +2,18 @@ package com.nexters.moss._base
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
+import androidx.annotation.ColorRes
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.ViewModel
 
-abstract class BaseActivity<T: ViewDataBinding> : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
     abstract val vm: ViewModel
 
     protected val binding by lazy {
@@ -20,18 +23,25 @@ abstract class BaseActivity<T: ViewDataBinding> : AppCompatActivity() {
     @LayoutRes
     abstract fun getLayoutRes(): Int
     abstract fun setupBinding()
+    @ColorRes
+    abstract fun getStatusBarColor(): Int
+
+    private fun setStatusBarColor(@ColorRes id: Int) {
+        window.statusBarColor = ContextCompat.getColor(this, id)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setStatusBarColor(getStatusBarColor())
         setupBinding()
         binding.lifecycleOwner = this
     }
 
-    inline fun<reified I : Activity> startActivity() {
+    inline fun <reified I : Activity> startActivity() {
         startActivity(Intent(applicationContext, I::class.java))
     }
 
-    inline fun<reified I : Activity> startActivityForResult(requestCode: Int) {
+    inline fun <reified I : Activity> startActivityForResult(requestCode: Int) {
         startActivityForResult(Intent(applicationContext, I::class.java), requestCode)
     }
 
